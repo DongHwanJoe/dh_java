8478134910c1d48e0507aa8e5989a375239978c7
dhjoe <ssd351@daum.net>
2022-10-25 15:39:02 +0900

    Step04 에 MainClass01 예제 추가함

--- Step04_Method/src/test/main/MainClass01.java
+++ Step04_Method/src/test/main/MainClass01.java
@@ -1,5 +1,6 @@
 package test.main;
 
+import test.mypac.Car;
 import test.mypac.MyObject;
 
 public class MainClass01 {
@@ -14,5 +15,26 @@ public class MainClass01 {
 		ob.walk();
 		int num = ob.getNumber();
 		String str = ob.getGreeting();
+		Car car = ob.getCar();
+		// setNum(), setName(), useCar() 메소드도 호출해보세요.
+		
+		//메소드 호출하면서 인자로 전달할 데이터를 즉석에서 만들어 전달하기
+		ob.setNum(10);
+		ob.setName("김구라");
+		ob.useCar(new Car());
+		
+		//메소드 호출하면서 인자로 전달할 값이 지역변수에 이미 존재하는 경우라고 가정하자
+		int a = 999;
+		String b = "누구게";
+		Car c1 = new Car();
+		
+		//전달할 값이 들어있는 변수명으로 전달 할 수 있다.
+		ob.setNum(a);
+		ob.setName(b);
+		ob.useCar(c1);
+		
+		// doSomething() 메소드를 호출해 보세요
+		ob.doSomething(10, "김구라", new Car());
+		ob.doSomething(a, b, c1);
 	}
 }
