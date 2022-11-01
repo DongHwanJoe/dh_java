b5152da2be80dc821fcfe4068aef7119632abfdc
dhjoe <ssd351@daum.net>
2022-11-01 17:24:56 +0900

    Step14_Swing 프로젝트 추가함

---
+++ Step14_Swing/src/frame01/MyFrame.java
@@ -0,0 +1,35 @@
+package frame01;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	//생성자
+	public MyFrame(String title) {
+		super(title);
+		//프레임의 초기 설정 작업하기
+		this.setBounds(100, 100, 500, 500);
+		// MyFrame의 x 버튼(close 버튼) 을 눌렀을 때 프로세스도 같이 종료되도록 설정
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 레이아웃 메니저는 아직 사용하지 않겠다 (모든 UI를 절대 좌표에 배치하겠다)
+		this.setLayout(null);
+		
+		//버튼 객체 만들기
+		JButton btn1 = new JButton("버튼1");
+		//위치 설정
+		btn1.setLocation(10, 10);
+		//크기 설정
+		btn1.setSize(100, 30);
+		//프레임에 추가하기
+		this.add(btn1);
+		
+		// 프레임을 화면상에 실제로 보이게하기(false 하면 화면에 보이지 않는다)
+		this.setVisible(true);
+	}
+	//run 했을 때 실행의 흐름이 시작되는 특별한 메소드
+	public static void main(String[] args) {
+		//MyFrame 객체 생성하기
+		new MyFrame("나의 프레임");
+	}
+}
